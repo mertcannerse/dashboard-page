567dcd84037e6f93c2ff38ed9a9d59ae7d89fd82
alitolga0 <cakiralitolga@gmail.com>
2023-08-08 10:18:26 +0300

    add row3 right side

--- js/row3-graphic.js
+++ js/row3-graphic.js
@@ -32,3 +32,185 @@ const row3config = {
     },
 };
 let totalOrder = new Chart(document.getElementById("totalOrder"), row3config);
+
+
+
+const row3data1 = {
+    labels: labels,
+    datasets: [
+        {
+            label: "",
+            data: [60, 40, 50, 80, 90, 70, 60, 40, 50, 70, 60, 80, 400],
+            fill: false,
+            borderColor: "#F87600",
+            tension: 0.1,
+            pointRadius: 0,
+        },
+    ],
+};
+const row3config1 = {
+    type: "line",
+    data: row3data,
+    options: {
+        scales: {
+            x: {
+                display: false,
+            },
+            y: {
+                display: false,
+            },
+        },
+        plugins: {
+            legend: {
+                display: false,
+            },
+        },
+    },
+};
+let totalOrder1 = new Chart(document.getElementById("totalOrder1"), row3config1);
+
+
+
+
+
+
+const row3data2 = {
+    labels: labels,
+    datasets: [
+        {
+            label: "",
+            data: [60, 40, 50, 80, 90, 70, 60, 40, 50, 70, 60, 80, 400],
+            fill: false,
+            borderColor: "#F87600",
+            tension: 0.1,
+            pointRadius: 0,
+        },
+    ],
+};
+const row3config2 = {
+    type: "line",
+    data: row3data,
+    options: {
+        scales: {
+            x: {
+                display: false,
+            },
+            y: {
+                display: false,
+            },
+        },
+        plugins: {
+            legend: {
+                display: false,
+            },
+        },
+    },
+};
+let totalOrder2 = new Chart(document.getElementById("totalOrder2"), row3config2);
+
+
+
+const row3data3 = {
+    labels: labels,
+    datasets: [
+        {
+            label: "",
+            data: [60, 40, 50, 80, 90, 70, 60, 40, 50, 70, 60, 80, 400],
+            fill: false,
+            borderColor: "#F87600",
+            tension: 0.1,
+            pointRadius: 0,
+        },
+    ],
+};
+const row3config3 = {
+    type: "line",
+    data: row3data,
+    options: {
+        scales: {
+            x: {
+                display: false,
+            },
+            y: {
+                display: false,
+            },
+        },
+        plugins: {
+            legend: {
+                display: false,
+            },
+        },
+    },
+};
+let totalOrder3 = new Chart(document.getElementById("totalOrder3"), row3config3);
+
+
+const row3data4 = {
+    labels: labels,
+    datasets: [
+        {
+            label: "",
+            data: [60, 40, 50, 80, 90, 70, 60, 40, 50, 70, 60, 80, 400],
+            fill: false,
+            borderColor: "#F87600",
+            tension: 0.1,
+            pointRadius: 0,
+        },
+    ],
+};
+const row3config4 = {
+    type: "line",
+    data: row3data,
+    options: {
+        scales: {
+            x: {
+                display: false,
+            },
+            y: {
+                display: false,
+            },
+        },
+        plugins: {
+            legend: {
+                display: false,
+            },
+        },
+    },
+};
+let totalOrder4 = new Chart(document.getElementById("totalOrder4"), row3config4);
+
+
+
+const row3data5 = {
+    labels: labels,
+    datasets: [
+        {
+            label: "",
+            data: [60, 40, 50, 80, 90, 70, 60, 40, 50, 70, 60, 80, 400],
+            fill: false,
+            borderColor: "#F87600",
+            tension: 0.1,
+            pointRadius: 0,
+        },
+    ],
+};
+const row3config5 = {
+    type: "line",
+    data: row3data,
+    options: {
+        scales: {
+            x: {
+                display: false,
+            },
+            y: {
+                display: false,
+            },
+        },
+        plugins: {
+            legend: {
+                display: false,
+            },
+        },
+    },
+};
+let totalOrder5 = new Chart(document.getElementById("totalOrder5"), row3config5);
